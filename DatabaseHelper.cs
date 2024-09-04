cf0317a1e10c71e11f268c7e0e903479e1529589
randallegend <randallofreneo@gmail.com>
2024-09-04 10:32:46 +0800

    Add project files.

---
+++ DatabaseHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STI_Join
+{
+    public static class DatabaseHelper
+    {
+        public const string connectionString = "Data Source=LAPTOP-SHP1G6JF\\SQLEXPRESS03;Initial Catalog=STIJoinDB;Integrated Security=True";
+    }
+}
